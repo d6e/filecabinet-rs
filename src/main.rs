f4fe46687f3679566d093ffeb4160644244a4126
Danielle Jenkins <git@d6e.io>
2020-08-13 02:43:29 -0700

    Add launch web option.

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ use clap::{Arg, App, value_t};
 
 struct Config {
     verbose: bool,
+    launch_web: bool,
     p_value: f32,
 }
 
 fn get_program_input() -> Config {
     let name_verbose = "verbose";
+    let name_launch_web = "web";
     let name_p_value = "pvalue";
     let default_p_value = 1.0;
     let matches = App::new("filecabinet")
@@ -25,6 +27,10 @@ fn get_program_input() -> Config {
             .long(name_verbose)
             .multiple(true)
             .help("Sets the level of verbosity"))
+        .arg(Arg::with_name(name_launch_web)
+            .short("w")
+            .long(name_launch_web)
+            .help("Launches the web server."))
         .arg(Arg::with_name(name_p_value)
             .short("p")
             .long("pvalue")
@@ -34,6 +40,7 @@ fn get_program_input() -> Config {
         .get_matches();
     Config {
         verbose: matches.is_present(name_verbose),
+        launch_web: matches.is_present(name_launch_web),
         p_value: value_t!(matches, name_p_value, f32).unwrap_or(default_p_value),
     }
 }
@@ -46,8 +53,8 @@ fn index() -> &'static str {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config = get_program_input();
-    rocket::ignite().mount("/", routes![index]).launch();
-
-    println!("Starting filecabinet...");
+    if config.launch_web {
+        rocket::ignite().mount("/", routes![index]).launch();
+    }
     Ok(())
 }
