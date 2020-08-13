25ab42bcf0c6f5d787df71379c829f0347c32222
Danielle Jenkins <git@d6e.io>
2020-08-13 15:19:24 -0700

    Add chacha20 encryption.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@
 #[macro_use] extern crate rocket;
 #[allow(dead_code)]
 use std::fs::{File, read_to_string};
-use std::{thread, time};
+use std::thread;
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::error::Error;
 use std::io::prelude::*;
 use clap::{Arg, App, value_t};
+use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use chacha20poly1305::aead::{Aead, NewAead};
+use rand::prelude::*;
+use blake3;
 
 struct Config {
     verbose: bool,
@@ -45,6 +50,54 @@ fn get_program_input() -> Config {
     }
 }
 
+fn mk_nonce() -> Nonce {
+    let mut rng = rand::thread_rng(); // TODO: maybe reuse
+    let rand_int: u128 = rng.gen();
+    let le = rand_int.to_le_bytes();
+    Nonce::clone_from_slice(&le[0..12]) // A 96-bit nonce (12 bytes); unique per message
+}
+
+fn hash_key(key: &str) -> blake3::Hash {
+    blake3::hash(key.as_bytes())
+}
+
+fn mk_cipher(key: blake3::Hash) -> ChaCha20Poly1305 {
+    let b: &[u8;32] = key.as_bytes();
+    let k = Key::from_slice(b); // 256-bit secret key (32 bytes)
+    ChaCha20Poly1305::new(k)
+}
+
+fn encrypt(cipher: &ChaCha20Poly1305, nonce: &Nonce, plaintext: &Vec<u8>) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
+    cipher.encrypt(&nonce, plaintext.as_slice())
+}
+
+fn decrypt(cipher: &ChaCha20Poly1305, nonce: &Nonce, ciphertext: Vec<u8>) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
+    cipher.decrypt(&nonce, ciphertext.as_ref())
+}
+
+#[test]
+fn test_encrypt_decrypt() {
+    let nonce = mk_nonce();
+    let s_key = "my key";
+    // Create two ciphers to test that recreating the cipher reliably works
+    let cipher1 = mk_cipher(hash_key(s_key));
+    let cipher2 = mk_cipher(hash_key(s_key));
+    let plaintext = String::from("my plaintext message").into_bytes();
+    let ciphertext = encrypt(&cipher1, &nonce, &plaintext).unwrap();
+    let decrypted_plaintext = decrypt(&cipher2, &nonce, ciphertext).unwrap();
+    assert_eq!(plaintext, decrypted_plaintext);
+}
+
+#[test]
+fn test_encrypt_decrypt_diff_key_fails() {
+    let nonce = mk_nonce();
+    let cipher1 = mk_cipher(hash_key("key1"));
+    let cipher2 = mk_cipher(hash_key("key2"));
+    let plaintext = String::from("my plaintext message").into_bytes();
+    let ciphertext = encrypt(&cipher1, &nonce, &plaintext).unwrap();
+    let result = decrypt(&cipher2, &nonce, ciphertext);
+    assert!(result.is_err());
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -53,6 +106,7 @@ fn index() -> &'static str {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config = get_program_input();
+
     if config.launch_web {
         rocket::ignite().mount("/", routes![index]).launch();
     }
