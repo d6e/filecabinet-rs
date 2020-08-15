0423e99259c867e35eb684a6f596870e1c971536
Danielle Jenkins <git@d6e.io>
2020-08-15 00:55:24 -0700

    Add cocoon.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use std::error::Error;
 use std::io::prelude::*;
 use clap::{Arg, App, value_t};
-use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
-use chacha20poly1305::aead::{Aead, NewAead};
-use rand::prelude::*;
-use blake3;
+use cocoon::Cocoon;
 
 struct Config {
     verbose: bool,
@@ -50,54 +47,15 @@ fn get_program_input() -> Config {
     }
 }
 
-fn mk_nonce() -> Nonce {
-    let mut rng = rand::thread_rng(); // TODO: maybe reuse
-    let rand_int: u128 = rng.gen();
-    let le = rand_int.to_le_bytes();
-    Nonce::clone_from_slice(&le[0..12]) // A 96-bit nonce (12 bytes); unique per message
-}
+// fn encrypt_file(file: &mut File, data: Vec<u8>) -> Result<(), Box<dyn Error>> {
+//     cocoon.dump(data, &mut file)?;
+//     Ok(())
+// }
 
-fn hash_key(key: &str) -> blake3::Hash {
-    blake3::hash(key.as_bytes())
-}
-
-fn mk_cipher(key: blake3::Hash) -> ChaCha20Poly1305 {
-    let b: &[u8;32] = key.as_bytes();
-    let k = Key::from_slice(b); // 256-bit secret key (32 bytes)
-    ChaCha20Poly1305::new(k)
-}
-
-fn encrypt(cipher: &ChaCha20Poly1305, nonce: &Nonce, plaintext: &Vec<u8>) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
-    cipher.encrypt(&nonce, plaintext.as_slice())
-}
-
-fn decrypt(cipher: &ChaCha20Poly1305, nonce: &Nonce, ciphertext: Vec<u8>) -> Result<Vec<u8>, chacha20poly1305::aead::Error> {
-    cipher.decrypt(&nonce, ciphertext.as_ref())
-}
-
-#[test]
-fn test_encrypt_decrypt() {
-    let nonce = mk_nonce();
-    let s_key = "my key";
-    // Create two ciphers to test that recreating the cipher reliably works
-    let cipher1 = mk_cipher(hash_key(s_key));
-    let cipher2 = mk_cipher(hash_key(s_key));
-    let plaintext = String::from("my plaintext message").into_bytes();
-    let ciphertext = encrypt(&cipher1, &nonce, &plaintext).unwrap();
-    let decrypted_plaintext = decrypt(&cipher2, &nonce, ciphertext).unwrap();
-    assert_eq!(plaintext, decrypted_plaintext);
-}
-
-#[test]
-fn test_encrypt_decrypt_diff_key_fails() {
-    let nonce = mk_nonce();
-    let cipher1 = mk_cipher(hash_key("key1"));
-    let cipher2 = mk_cipher(hash_key("key2"));
-    let plaintext = String::from("my plaintext message").into_bytes();
-    let ciphertext = encrypt(&cipher1, &nonce, &plaintext).unwrap();
-    let result = decrypt(&cipher2, &nonce, ciphertext);
-    assert!(result.is_err());
-}
+// fn decrypt_file(file: &mut File) -> Result<(), Box<dyn Error>>  {
+//     let data = cocoon::parse(&mut file)?;
+//     Ok(())
+// }
 
 #[get("/")]
 fn index() -> &'static str {
@@ -106,6 +64,16 @@ fn index() -> &'static str {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config = get_program_input();
+
+    let cocoon = Cocoon::new(b"password");
+    let mut file = File::create("foo.cocoon")?;
+    cocoon.dump("myplaintext".as_bytes().to_vec(), &mut file).unwrap();
+
+    let data = cocoon.parse(&mut file).unwrap();
+    let mut unencrypted_file = File::create("foo.txt")?;
+    let mut contents = vec![];
+    file.read_to_end(&mut contents)?;
+    unencrypted_file.write_all(&contents);
 
     if config.launch_web {
         rocket::ignite().mount("/", routes![index]).launch();
