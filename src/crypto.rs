8fb0511a5e1b7be56e587f7718b19014354a08ee
Danielle Jenkins <git@d6e.io>
2020-11-07 22:17:07 -0800

    Add integration test for encrypt_decrypt.

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,16 +1,120 @@
 use cocoon::Cocoon;
 use std::fs::File;
 use std::error::Error;
+use std::convert::AsRef;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use std::env;
 
+pub const ENCRYPTION_FILE_EXT: &str = ".cocoon";
 
-pub fn encrypt_file(password: &str, file: &mut File, data: Vec<u8>) -> Result<(), Box<dyn Error>> {
+fn encrypt(password: &str, file: &mut File, data: Vec<u8>) -> Result<(), Box<dyn Error>> {
     let cocoon = Cocoon::new(password.as_bytes());
     cocoon.dump(data, file).unwrap();
     Ok(())
 }
 
-pub fn decrypt_file(password: &str, file: &mut File) -> Result<Vec<u8>, Box<dyn Error>> {
+fn decrypt(password: &str, file: &mut File) -> Result<Vec<u8>, Box<dyn Error>> {
     let cocoon = Cocoon::new(password.as_bytes());
     let data = cocoon.parse(file);
     Ok(data.unwrap())
+}
+
+pub fn decrypt_file(path: &str, password: &str) -> Result<(), String> {
+    if path.ends_with(ENCRYPTION_FILE_EXT) {
+        let decrypted_path = get_decrypted_name(path);
+        let mut encrypted_file = File::open(path).unwrap();
+        let data = decrypt(password, &mut encrypted_file).unwrap();
+        let mut unecrypted: File = File::create(decrypted_path).unwrap();
+        unecrypted.write(&data).unwrap();
+        return Ok(());
+    } else {
+        return Err(format!("Error: '{}' is not encrypted.", path));
+    }
+}
+
+pub fn encrypt_file(path: &str, password: &str) -> Result<(), String> {
+    if ! path.ends_with(ENCRYPTION_FILE_EXT) {
+        let encrypted_path = get_encrypted_name(path);
+        let mut unencrypted = File::open(path).unwrap();
+        let mut buffer = Vec::new();
+        unencrypted.read_to_end(&mut buffer).unwrap();
+        let encrypted_file = &mut File::create(encrypted_path.clone()).unwrap();
+        encrypt(password, encrypted_file, buffer).unwrap();
+        return Ok(());
+    } else {
+        return Err(format!("Error: '{}' is already encrypted.", path));
+    }
+}
+
+pub fn get_decrypted_name<T: AsRef<Path>>(path: T) -> PathBuf {
+    let path = path.as_ref();
+    let mut filename = path.file_name().unwrap().to_str().unwrap().to_string();
+    let basepath = path.parent().unwrap();
+    // if it ends with the encryption extension, remove it.
+    if filename.ends_with(ENCRYPTION_FILE_EXT) {
+        filename = filename.replace(ENCRYPTION_FILE_EXT, "");
+    }
+    return basepath.join(filename);
+}
+
+pub fn get_encrypted_name<T: AsRef<Path>>(path: T) -> PathBuf {
+    let path = path.as_ref();
+    let mut filename = path.file_name().unwrap().to_str().unwrap().to_string();
+    let basepath = path.parent().unwrap();
+    // if it doesn't end with the encryption extension, add it.
+    if ! filename.ends_with(ENCRYPTION_FILE_EXT) {
+        filename.push_str(ENCRYPTION_FILE_EXT);
+    }
+    return basepath.join(filename);
+}
+
+#[test]
+fn test_get_decrypted_name() {
+    assert_eq!(get_decrypted_name(Path::new("/boop/loop/readme.md.cocoon")), Path::new("/boop/loop/readme.md"));
+    assert_eq!(get_decrypted_name(Path::new("readme.md.cocoon")), Path::new("readme.md"));
+}
+#[test]
+fn test_get_encrypted_name() {
+    assert_eq!(get_encrypted_name(Path::new("/boop/loop/readme.md")), Path::new("/boop/loop/readme.md.cocoon"));
+    assert_eq!(get_encrypted_name(Path::new("readme.md")), Path::new("readme.md.cocoon"));
+}
+
+#[test]
+fn test_encrypt_decrypt_file() {
+    // Create temporary test dir.
+    let mut tempdir = env::temp_dir();
+    tempdir.push("sdfasfd");
+    std::fs::create_dir_all(&tempdir).unwrap();
+    let data = b"hello world";
+
+    // Create test file and write some data to it.
+    let mut clear_text = tempdir.clone();
+    clear_text.push("test.txt");
+    let mut f: File = File::create(&clear_text).unwrap();
+    f.write_all(data).unwrap();
+    drop(f);
+
+    println!("{:?}", tempdir);
+
+    // Encrypt it.
+    encrypt_file(clear_text.to_str().unwrap(), "password").unwrap();
+
+    // Delete old file.
+    std::fs::remove_file(&clear_text).unwrap();
+
+    // Decrypt it.
+    let mut cipher_text = tempdir.clone();
+    cipher_text.push("test.txt.cocoon");
+    decrypt_file(cipher_text.to_str().unwrap(), "password").unwrap();
+
+    // Verify file was encrypted and decrypted without loss.
+    let mut f = File::open(&clear_text).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+
+    assert_eq!(data.to_vec(), buffer);
+
+    // Cleanup
+    std::fs::remove_dir_all(tempdir).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,15 @@ use std::io::prelude::*;
 use rocket_contrib::serve::StaticFiles;
 use chrono::{DateTime, Utc};
 use serde::Serialize;
-use std::convert::AsRef;
 use std::ffi::OsStr;
 use rocket::response::Redirect;
 use regex::Regex;
+use rocket::response::{Response, Responder};
 use std::fs;
 
 mod crypto;
 mod cli;
 mod checksum;
-
-const ENCRYPTION_FILE_EXT: &str = ".cocoon";
 
 #[derive(FromForm, Clone)]
 struct Document {
@@ -48,28 +46,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // If there's a specific file we should decrypt, do that.
     if let Some(path) = &config.file_to_decrypt {
-        if path.ends_with(ENCRYPTION_FILE_EXT) {
-            let decrypted_path = get_decrypted_name(path);
-            let mut encrypted_file = File::open(path).unwrap();
-            let data = crypto::decrypt_file(&pass, &mut encrypted_file).unwrap();
-            let mut unecrypted: File = File::create(decrypted_path).unwrap();
-            unecrypted.write(&data).unwrap();
-        } else {
-            println!("Error: '{}' is not encrypted.", path);
+        if let Err(s) = crypto::decrypt_file(path, pass) {
+            println!("{}", s);
         }
     }
 
     // If there's a specific file we should encrypt, do that too.
     if let Some(path) = &config.file_to_encrypt {
-        if ! path.ends_with(ENCRYPTION_FILE_EXT) {
-            let encrypted_path = get_encrypted_name(path);
-            let mut unencrypted = File::open(path)?;
-            let mut buffer = Vec::new();
-            unencrypted.read_to_end(&mut buffer)?;
-            crypto::encrypt_file(&pass, &mut File::create(encrypted_path.clone())?, buffer)?;
-            checksum::generate_sha256(encrypted_path).unwrap();
-        } else {
-            println!("Error: '{}' is already encrypted.", path);
+        match crypto::encrypt_file(path, pass) {
+            Err(s) => println!("{}", s),
+            Ok(_) => checksum::generate_sha256(Path::new(path).to_path_buf()).unwrap()
         }
     }
 
@@ -114,30 +100,27 @@ fn get_docs(config: State<cli::Config>) -> JsonValue {
 fn get_doc(config: State<cli::Config>, filename: String) -> Template {
     let now: DateTime<Utc> = Utc::now();
     let files: Vec<String> = list_files(&PathBuf::from(&config.target_directory));
-    if filename.ends_with(".cocoon") {
+    if filename.ends_with(crypto::ENCRYPTION_FILE_EXT) {
         // If file is encrypted, decrypt to temporary dir and return new file
-        let mut encrypted: File = File::open(Path::new(&config.target_directory).join(filename.clone())).unwrap();
+        let path = Path::new(&config.target_directory).join(filename.clone());
         let pass = config.password.clone().unwrap();
-        let data = crypto::decrypt_file(&pass, &mut encrypted).unwrap();
-        let mut unencrypted_path = Path::new(&config.target_directory).join("tmp");
-        if !unencrypted_path.exists() {
-            fs::create_dir(unencrypted_path.clone()).unwrap();
+        if let Err(e) = crypto::decrypt_file(path.to_str().unwrap(), &pass) {
+            // TODO: Return appropriate HTTP error in response.
         }
-        let unencrypted_name = filename.replace(".cocoon", ".pdf");
-        unencrypted_path.push(unencrypted_name.clone());
-        let mut unecrypted: File = File::create(unencrypted_path.clone()).unwrap();
-        unecrypted.write(&data).unwrap();
-
-        let date = parse_date(&unencrypted_name).unwrap();
+        let unencrypted_path: PathBuf = crypto::get_decrypted_name(path);
+        let unencrypted_name = unencrypted_path.file_name().unwrap().to_str().unwrap();
+        let date = parse_date(unencrypted_name).unwrap();
         let doc = to_document(&unencrypted_path.to_str().unwrap());
+
+        let target_directory = unencrypted_path.parent().unwrap().to_str().unwrap().to_owned().replace("/", "\\u{002F}");
         let new_context = Context {
-            filename: unencrypted_name,
+            filename: unencrypted_name.to_string(),
             name: doc.name.unwrap_or(String::new()),
             date: doc.date.unwrap_or(date),
             institution: doc.institution.unwrap_or(String::new()),
             page: doc.page.unwrap_or(String::from("1")),
             files: files,
-            target_directory: unencrypted_path.parent().unwrap().to_str().unwrap().to_owned().replace("/", "\\u{002F}")
+            target_directory: target_directory
         };
         return Template::render("index", &new_context);
     } else {
@@ -158,14 +141,11 @@ fn get_doc(config: State<cli::Config>, filename: String) -> Template {
 
 #[post("/doc", data = "<doc>")]
 fn new(config: State<cli::Config>, doc: Form<Document>) -> Result<Redirect, Box<dyn Error>> {
-    let file_to_write = Path::new(&config.target_directory)
+    let filename = Path::new(&config.target_directory)
         .join(format!("{}_{}_{}_{}.cocoon", doc.date, doc.institution, doc.name, doc.page));
-    let mut unencrypted = File::open(Path::new(&config.target_directory).join(&doc.filename))?;
-    let mut buffer = Vec::new();
-    unencrypted.read_to_end(&mut buffer)?;
-    let checksum_name = file_to_write.clone();
+    let checksum_name = filename.clone();
     let password = config.password.clone().unwrap();
-    crypto::encrypt_file(&password, &mut File::create(file_to_write)?, buffer)?;
+    crypto::encrypt_file(filename.to_str().unwrap(), &password)?;
     checksum::generate_sha256(checksum_name).unwrap();
     Ok(Redirect::to("/"))
 }
@@ -213,16 +193,6 @@ fn parse_date(text: &str) -> Option<String> {
     )
 }
 
-#[test]
-fn test_parse_date_hyphens() {
-    assert_eq!(parse_date("2020-04-03_boop_loop"), Some("2020-04-03".to_string()))
-}
-
-#[test]
-fn test_parse_date_no_hyphens() {
-    assert_eq!(parse_date("20180530_boop_loop"), Some("2018-05-30".to_string()))
-}
-
 fn get_filestem_from_filename(filename: &str) -> Option<&str> {
     Path::new(filename)
         .file_stem()
@@ -241,34 +211,11 @@ fn to_document(filename: &str) -> OptDoc {
 }
 
 #[test]
-fn test_get_decrypted_name() {
-    assert_eq!(get_decrypted_name(Path::new("/boop/loop/readme.md.cocoon")), Path::new("/boop/loop/readme.md"));
-    assert_eq!(get_decrypted_name(Path::new("readme.md.cocoon")), Path::new("readme.md"));
+fn test_parse_date_hyphens() {
+    assert_eq!(parse_date("2020-04-03_boop_loop"), Some("2020-04-03".to_string()))
 }
+
 #[test]
-fn test_get_encrypted_name() {
-    assert_eq!(get_encrypted_name(Path::new("/boop/loop/readme.md")), Path::new("/boop/loop/readme.md.cocoon"));
-    assert_eq!(get_encrypted_name(Path::new("readme.md")), Path::new("readme.md.cocoon"));
-}
-
-fn get_decrypted_name<T: AsRef<Path>>(path: T) -> PathBuf {
-    let path = path.as_ref();
-    let mut filename = path.file_name().unwrap().to_str().unwrap().to_string();
-    let basepath = path.parent().unwrap();
-    // if it ends with the encryption extension, remove it.
-    if filename.ends_with(ENCRYPTION_FILE_EXT) {
-        filename = filename.replace(ENCRYPTION_FILE_EXT, "");
-    }
-    return basepath.join(filename);
-}
-
-fn get_encrypted_name<T: AsRef<Path>>(path: T) -> PathBuf {
-    let path = path.as_ref();
-    let mut filename = path.file_name().unwrap().to_str().unwrap().to_string();
-    let basepath = path.parent().unwrap();
-    // if it doesn't end with the encryption extension, add it.
-    if ! filename.ends_with(ENCRYPTION_FILE_EXT) {
-        filename.push_str(ENCRYPTION_FILE_EXT);
-    }
-    return basepath.join(filename);
+fn test_parse_date_no_hyphens() {
+    assert_eq!(parse_date("20180530_boop_loop"), Some("2018-05-30".to_string()))
 }
